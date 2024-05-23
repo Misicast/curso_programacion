4579a88498d854915a4a86fab2b63210484e8cde
mirna <mirnadjsc@gmail.com>
2024-05-23 12:24:19 -0400

    practica23mayÇ

---
+++ practica23may.py
@@ -0,0 +1,13 @@
+#EJERCICIO 1
+print("hola mundo")
+
+#EJERCICIO 2
+MENSAJE= "HOLA MUNDO"
+print(MENSAJE)
+
+#EJERCICIO 3
+print("¿Cuál es tu Nombre?")
+Nombre= input()
+print ("Mucho gusto," Nombre)
+
+#EJERCICIO 4
